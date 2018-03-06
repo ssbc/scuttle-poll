e91df8e00ca174250db3f719cc4bbf9a3b54b07c
Piet Geursen <pietgeursen@gmail.com>
2018-03-06 15:02:05 +1300

    Uses lodash.clonedeep to avoid modifying schema.

--- poll/schema/chooseOnePoll.js
+++ poll/schema/chooseOnePoll.js
@@ -1,6 +1,7 @@
 const pollSchema = require('./poll')
+const cloneDeep = require('lodash.clonedeep')
 
-const chooseOneSchema = Object.assign({}, pollSchema)
+const chooseOneSchema = cloneDeep(pollSchema)
 
 // collapse the details down to be ONLY chooseOne
 chooseOneSchema.properties.pollDetails = { $ref: '#/definitions/pollDetails/chooseOne' }
