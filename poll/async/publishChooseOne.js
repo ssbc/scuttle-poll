3f4d140a4dd5dc3eb9a025d3d8a9e6272156a067
mix irving <whimful@gmail.com>
2018-05-04 16:11:30 +1200

    fix error reporter

--- poll/async/publishChooseOne.js
+++ poll/async/publishChooseOne.js
@@ -1,10 +1,10 @@
 const ChooseOne = require('../sync/buildChooseOne')
-const { isPoll } = require('ssb-poll-schema')
+const { isPoll, getPollErrors } = require('ssb-poll-schema')
 
 module.exports = function (server) {
   return function publishChooseOne (opts, cb) {
     const poll = ChooseOne(opts)
-    if (!isPoll.chooseOne(poll)) return cb(isPoll.chooseOne.errors)
+    if (!isPoll.chooseOne(poll)) return cb(getPollErrors(poll))
 
     server.publish(poll, cb)
   }
